832260b8c3175b324551c969cf7fb8ed7c869966
ZeinabMoayeri <z.moayeri.k@gmail.com>
2024-10-02 13:24:00 +0330

    update 5

--- main.py
+++ main.py
@@ -6,11 +6,3 @@ from keras.layers import Dense
 import numpy as np
 
 from dataset import load_hoda
-''' jfndsjvbdfjhvbdfjhbvad 
-vjhbvjfdhbvhdjcnv
-jhdfbvjhdfbvhjbd
-jhbvjhdfvhjbd
-cjksdjcnjndekjdlskaxd
-djdskjfwkjsnd
-jwndkjsab
-'''
